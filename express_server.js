cddcd001cdcd7704ab9469df4892f5f41172b727
maksym <maksym.polyakov@yahoo.ca>
2024-04-18 18:11:19 -0500

    add ejs test

--- express_server.js
+++ express_server.js
@@ -25,3 +25,8 @@ app.listen(PORT, () => {
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
+
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
